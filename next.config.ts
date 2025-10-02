c1e0059ace248d2ae821b1001ad73d93e290f87f
mohammedfarish <farishmusthafa@gmail.com>
2025-10-02 15:18:23 +0400

    minor update --prod

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [{ key: "X-Frame-Options", value: "DENY" }],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
